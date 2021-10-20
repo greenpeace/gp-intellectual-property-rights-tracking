3d82357760dfc14653efd6e982cc1f2bb49512c3
Ankebod <ankebodewes@outlook.com>
2021-10-20 18:14:26 +0200

    update and change logging

--- gp-techlab-spreadshirt/src/main.py
+++ gp-techlab-spreadshirt/src/main.py
@@ -2,11 +2,17 @@ import logging
 import requests
 from bs4 import BeautifulSoup
 import re
+import urllib
 
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-from config import PROJECT
+# configure local or cloud
+try:
+    from config import PROJECT # only cloud
+except:
+    PROJECT = 'torbjorn-zetterlund' # only local
+    logging.basicConfig(filename='test.log', level=logging.INFO) # log only local
 
 # initialize firebase sdk
 CREDENTIALS = credentials.ApplicationDefault()
@@ -25,103 +31,80 @@ def main(request):
     # Fake Real Browser
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'}
 
-    searchlink_ref = db.collection(u'searchlinks')
+    searchquery_ref = db.collection(u'searchquery')
     
-    for doc in searchlink_ref.where(u'shop', u'==', 'spreadshirt').stream():
-        url = u'{}'.format(doc.to_dict()['url'])
-        shop = u'{}'.format(doc.to_dict()['shop'])
-        #        url = cleanurl(url)
-        print(url)
+    total = 0
+    
+    for doc in searchquery_ref.where(u'active', u'==', True).stream():
+        query = u'{}'.format(doc.to_dict()['queryterm'])
+        query = urllib.parse.quote_plus(query)
 
+        # start at the first search tab
+        url = 'https://www.spreadshirt.com/shop/' + query
+
+        counter = 0
         try:
             page = requests.get(url, headers=headers)
 
             # Get web page
-            spreadshirt = BeautifulSoup(page.content, "html5lib")
-            # Regular Expression for a css class that ha dynamic values
-            regex = re.compile('col-6 col-md-4')
-            regexid = re.compile('l-*')
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            products = soup.find_all('a', class_='article thumb-font article--label')
+
             # Get All links in the css class defined by regex
-            for divs in spreadshirt.find_all(id=regexid, class_=regex):
-                item_url = divs.find("a")  
-                item_url = item_url['href']
-                #item_url = cleanurl(item_url)
-                item_url = convert(item_url)
-                #print(divs)
+            for product in products: 
+                item_url = convert(product['href'])
+
                 # Duplicate check
                 docsurl = db.collection(u'illegalmerchandise').where(u'item_url', u'==', item_url).stream()
-                if (len(list(docsurl))):
-                    logging.info("URL Exist, we will ignore")
-                else:
-                    logging.info("URL Not found, we will add to databse")
+                if not (len(list(docsurl))):
 
-                    item_image_title = divs.find('img')
-                    item_image_title = item_image_title['alt']
+                    item_image = product.find('img')
+                    item_image_title = item_image['alt']
 
-                    # Check if Keywords Exixst in Product title
+                    # Check if Keywords Exist in Product title
                     searchkeywords_ref = db.collection(u'searchquerykeywords')        
                     # Request data from Firestore
                     for doc in searchkeywords_ref.where(u'active', u'==', True).stream():
                         keywords.append(u'{}'.format(doc.to_dict()['querykeywords']))
                         
                     if any(x in item_image_title for x in keywords):
-                        try:
-                            item_image_url = divs.find('img')
-                            item_image_url = item_image_url['src']
-                        except:
-                            item_image_url = item_image_url['src']
+                                               
+                        item_image_url = item_image['src']
 
-                        item_image_url = convert(item_image_url)
-                        # Get data from sub page
-                        subpage = requests.get(item_url, headers=headers)
+                        # Rest of the data is behind dynamic JavaScript:(
+       
+                        seller = ''
+                        store_url = ''
+                        location = ''
+                        shop = 'spreadshirt'
+                        contact_seller = ''
 
-                        spreadshirtsoup = BeautifulSoup(subpage.text, "html5lib")
-
-                        for subdivs in spreadshirtsoup.find_all("div", class_="detail-header__designer-link"):
-
-                            sellerdetail = subdivs.find("a")    
-                            #  for link in ebaysoup.find_all("a"):
-                            try:
-                                store_url = sellerdetail['href']
-                                store_url = convert(store_url)
-                            except:
-                                store_url = ''
-
-                            #  for link in ebaysoup.find_all("a"):
-                            try:
-                                seller = sellerdetail.text
-                            except:
-                                seller = ''
-
-                            # Get data from shop page
-                            try:
-                                contact_seller = subdivs.find('div', class_='si-pd-a').a['href']
-                            except:
-                                contact_seller = ''
-
-                            shop = 'Spreadshirt'
-                            location = ''
-
-                            data = {
-                                'contact_seller': contact_seller,
-                                'item_image_title': item_image_title,
-                                'item_image_url': item_image_url,
-                                'item_url': item_url,
-                                'location': location,
-                                'seller': seller,
-                                'shop': shop,
-                                'site': url,
-                                'status': True,
-                                'store_url': store_url,
-                                'note': ''
-                            }
-                            db.collection('illegalmerchandise').document().set(data)  # Add a new doc in collection links with ID shop
+                        data = {
+                            'contact_seller': contact_seller,
+                            'item_image_title': item_image_title,
+                            'item_image_url': item_image_url,
+                            'item_url': item_url,
+                            'location': location,
+                            'seller': seller,
+                            'shop': shop,
+                            'site': url,
+                            'status': True,
+                            'store_url': store_url,
+                            'note': ''
+                        }
+                        db.collection('illegalmerchandise').document().set(data)  # Add a new doc in collection links with ID shop
+                        counter += 1
+                        
                     else:
-                        logging.info("No match on Keywords")    # -> <match object>
-        except:
-            logging.info("Error Getting main product page")
+                        logging.info(f"No match on Keywords for {item_image_title}")    # -> <match object>
+            
+            total += counter 
+            logging.info(f"We added {counter} links for search term {query}")   
+        except Exception as e:
+            logging.info(f"Error Getting main product page: {e}")
    
-    return "All Done"
+    return f"All Done, we added {total} links"
 
 def cleanurl(url):
     matches = re.findall('(.+\?)([^#]*)(.*)', url)
@@ -143,3 +126,9 @@ def convert(url):
     if not url.startswith('http://'):
         return 'http://' + url
     return url
+
+# call function, only needed locally
+try:
+    from config import PROJECT # only cloud
+except:
+    main('request')
