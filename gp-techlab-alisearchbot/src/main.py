b9e3fd38824c2c6f5c68ed456a43fb795a702636
Ankebod <ankebodewes@outlook.com>
2021-10-27 17:11:38 +0200

    update requirements for firebase admin dependency

--- gp-techlab-alisearchbot/src/main.py
+++ gp-techlab-alisearchbot/src/main.py
@@ -1,9 +1,5 @@
 import logging
 import re
-<<<<<<< HEAD
-=======
-from requests_html import HTMLSession 
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
 import firebase_admin
 from firebase_admin import credentials, firestore
 from pyppeteer import launch
@@ -29,7 +25,6 @@ firebase_admin.initialize_app(CREDENTIALS, {
 # get firestore client
 db = firestore.client()
 
-<<<<<<< HEAD
 async def main(request):
     
     # Database Details
@@ -43,26 +38,11 @@ async def main(request):
     for doc in searchquery_ref.where(u'active', u'==', True).stream():
         query = u'{}'.format(doc.to_dict()['queryterm'])
         query = query.replace(" ", "+")
-=======
-def main(request):
-    
-    # Database Details
-    searchlink_ref = db.collection(u'searchlinks')
-
-    # Empty list to store keywords in
-    keywords = [] 
-
-    # Read from Database the aliexpress url that are active
-    for doc in searchlink_ref.where(u'status', u'==', True).where(u'shop', u'==', 'aliexpress').stream():    
-        url = u'{}'.format(doc.to_dict()['url'])
-        shop = u'{}'.format(doc.to_dict()['shop'])
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
         counter = 0
 
         url = 'https://www.aliexpress.com/w/wholesale-' + query + '.html'
         try:
             # Start a session
-<<<<<<< HEAD
             browser = await launch(headless=True, ignoreHTTPSErrors=True, args=['--no-sandbox'])
             page = await browser.newPage()
             await page.goto(url)
@@ -81,27 +61,6 @@ def main(request):
                 # Grab image title
                 i = await product.querySelector('img.A3Q1M[alt]')
                 item_image_title = await page.evaluate('(element) => element.alt', i)
-=======
-            session = HTMLSession()
-
-            # Get the webpage
-            r = session.get(url)
-            r.html.render(sleep = 1, keep_page=True, scrolldown=10) # Set scroll down 10 to catch all products
-            divs = r.html
-
-            # Get products
-            products = divs.find('._1OUGS')
-
-            for product in products:
-                # Grab item url
-                item_attrs = product.find('a._9tla3')[0].attrs         
-                item_url = item_attrs['href'] 
-                item_url = cleanurl(item_url)
-                
-                # Grab image title
-                image_attrs = product.find('img.A3Q1M')[0].attrs # Retrieve image div
-                item_image_title = image_attrs['alt']
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
 
                 # Duplicate check
                 docsurl = db.collection(u'illegalmerchandise').where(u'item_url', u'==', item_url).stream()
@@ -112,11 +71,7 @@ def main(request):
                 
                 else:
                     logging.info("URL Not found, we will add to database")
-<<<<<<< HEAD
                         
-=======
-                    
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
                     # Retrieve keywords to check them in product title
                     searchkeywords_ref = db.collection(u'searchquerykeywords')        
                     # Request data from Firestore
@@ -133,31 +88,18 @@ def main(request):
 
                     # Otherwise add to database    
                     else:
-<<<<<<< HEAD
                         u = await product.querySelector('img.A3Q1M[src]')
                         item_image_url = await page.evaluate('(element) => element.src', u) 
                         
                         try:
                             u = await product.querySelector('a._2lsU7[href]')
                             store_url = await page.evaluate('(element) => element.href', u) 
-=======
-                        item_image_url = image_attrs['src']
-                        
-                        sellerdetail = product.find("a._2lsU7")[0]    
-
-                        try:
-                            store_url = sellerdetail.attrs['href']
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
                         except:
                             store_url = ''
 
                         try:
-<<<<<<< HEAD
                             u = await product.querySelector('a._2lsU7')
                             seller = await page.evaluate('(element) => element.textContent', u) 
-=======
-                            seller = sellerdetail.text
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
                         except:
                             seller = ''
 
@@ -183,7 +125,6 @@ def main(request):
                     
                         db.collection('illegalmerchandise').document().set(data)  # Add a new doc in collection links with ID shop
                         counter += 1
-<<<<<<< HEAD
                         logging.info(f"Added a product")                   
                         
             info = f"Added {counter} products to the database for url {url}"
@@ -211,29 +152,3 @@ def main(request):
 # asyncio.get_event_loop().run_until_complete(main('request')) # only local
 
 #  main('request') # only local
-=======
-                        
-            info = f"Added {counter} products to the database for url {url}"
-            logging.info(info)
-            session.close()
-
-        except:
-            logging.info(f"Error getting main product page, response was {r.status_code}")
-   
-    return "All Done"
-
-def cleanurl(url):
-    base = 'https://aliexpress.com'
-    
-    # Remove part after question mark
-    matches = re.findall('(.+\?)([^#]*)(.*)', url)
-
-    if len(matches) == 0:
-        newurl = base + url
-    else:
-        newurl = base + matches[0][0][:-1] # We take all but the last character (the ?) from the first item in the tuple that is the first item of the matches list
-    
-    return newurl
-
-# main('request') # only local
->>>>>>> 604c57d61ce29689b03b4ee7669bd1dfd2b66f48
