15b4cf314883e2622ea5118f068cfaf79392ae4e
Ankebod <ankebodewes@outlook.com>
2021-12-08 16:10:23 +0100

    comment out product page search

--- gp-techlab-ebaysearchbot/src/index.js
+++ gp-techlab-ebaysearchbot/src/index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue, WriteBatch } = require('firebase-admin/firestore');
 const {PubSub} = require('@google-cloud/pubsub');
+const { logger } = require('firebase-functions/v1');
 // const serviceAccount = require('/home/ankebod/gp-intellectual-property-rights-tracking/torbjorn-zetterlund-e9d45e69e6a8.json');
 
 
@@ -27,7 +28,6 @@ exports.main = async (req, res) => {
         for (const query of queryArray) {  
 
             var url = 'https://www.ebay.com/sch/i.html?_&_nkw=' + query
-            console.log(url);
             
             // Open a page
             const browser = await browserPromise;
@@ -40,40 +40,59 @@ exports.main = async (req, res) => {
             try {
                 await page.waitForSelector('a.s-item__link');
 
-                // Scrape the page
+                // Scrape the result page
                 let results = await page.evaluate(() => {
                     var base = 'https://www.ebay.com'
                     var productList = document.querySelectorAll('a.s-item__link[target]');
                     var imageList = document.querySelectorAll('img.s-item__image-img');
-                    var locationList = document.querySelectorAll('span.s-item__location.s-item__itemLocation');
                     var resultArray = [];
 
-                    for (var i = 0; i < productList.length; i++){
-                        try {
-                            var loc = locationList[i].innerHTML.trim().split(' ');
-                            loc.shift()
-                            loc = loc.join(' ')
-                        }
-                        catch {
-                            var loc = ''
-                        }
-                        
+                    for (var i = 0; i < productList.length; i++){                        
                         resultArray[i] = {
                             contact_seller : '',
                             item_image_title : imageList[i].getAttribute('alt'),
                             item_image_url : imageList[i].getAttribute('src'),
                             item_url : productList[i].getAttribute('href'),
-                            location : loc,
-                            seller : '',
                             shop : 'ebay',
                             site : base,
                             status : false,
-                            store_url : '',
-                            note : ''
+                            note : '',
+                            location : '',
+                            seller : '',
+                            store_url : ''
                         }
+                      
                     }
                     return resultArray
                 })
+
+                // // Scrape the product pages
+                // for (var i = 0; i < results.length; i++){
+                //     var link = results[i]['item_url']
+                //     startTime = new Date();
+                //     console.log(link);
+                //     let site = await fulfillWithTimeLimit(2000, page.goto(link), null);
+                    
+                //     // await page.waitForSelector('div.iti-eu-bld-gry span[itemprop]');
+                //     if(site != null){
+                //         let info = await page.evaluate(() => {
+                //             var infoArray = []
+
+                //             infoArray = {
+                //                 location : document.querySelector('div.iti-eu-bld-gry span[itemprop]').innerHTML.trim(),
+                //                 seller : document.querySelector('div.bdg-90 a span.mbg-nw').innerHTML.trim(),
+                //                 store_url : document.querySelector('div.si-pd-a a')['href']
+                //             }
+
+                //             return infoArray
+                //         })    
+
+                //         results[i]['location'] = info['location']
+                //         results[i]['seller'] = info['seller']
+                //         results[i]['store_url'] = info['store_url']
+                //         endTime = new Date();
+                //     }
+                // } 
                 
                 // Store the results  
                 const merchDb = db.collection('illegalmerchandise');
@@ -133,4 +152,18 @@ async function pubMessage(topic){
 
     const messageBuffer = Buffer.from(JSON.stringify({data: 'Run Selector'}));
     await topic.publish(messageBuffer)
+}
+
+async function fulfillWithTimeLimit(timeLimit, task, failureValue){
+    let timeout;
+    const timeoutPromise = new Promise((resolve, reject) => {
+        timeout = setTimeout(() => {
+            resolve(failureValue);
+        }, timeLimit);
+    });
+    const response = await Promise.race([task, timeoutPromise]);
+    if(timeout){ //the code works without this but let's be safe and clean up the timeout
+        clearTimeout(timeout);
+    }
+    return response;
 }
